bbadf900ffca4916c0f8ea6abab700334e7fe124
Batuhan337tr <47800213+Batuhan337tr@users.noreply.github.com>
2021-03-02 15:03:19 +0300

    Add files via upload

---
+++ hashs.php
@@ -0,0 +1,11 @@
+{
+"MAIN": {
+"launcher.jar": "dca5ed5613e76aaaec6fa0c85c9ce73f9476a5b7"
+},
+"WINDOWS_BS": {
+"javaURL": "http:\/\/client.craftrise.network\/api\/launcher\/launcher.json",
+"javaType": "jdk",
+"launcherURL": "http://nevarneyok.me/FPGenerator/launcher.jar",
+"startArguments": "-Xmx%selectedRAM%M -Djava.net.preferIPv4Stack=true -XX:+DisableAttachMechanism -Dcom.ibm.tools.attach.enable=no -Xmn128M"
+}
+}
